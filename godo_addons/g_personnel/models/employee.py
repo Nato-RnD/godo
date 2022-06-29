9b284f8d103c32c4a6b03e7b1ddd7c07e1525ffd
Wong Golden <thiep.wong@gmail.com>
2022-06-29 17:00:39 +0700

    cap nhat

--- godo_addons/g_personnel/models/employee.py
+++ godo_addons/g_personnel/models/employee.py
@@ -2,8 +2,4 @@ from odoo import models, fields, api,_
 
 class HrEmployee(models.Model): 
     _inherit = 'hr.employee'
-    
-    
-    province_id = fields.Many2one(comodel_name='res.province', string='Tỉnh/Thành')
-    district_id = fields.Many2one(comodel_name='res.district', string='Huyện/Quận')
-    ward_id = fields.Many2one(comodel_name='res.province', string='Xã/Phường')
+   
---
+++ godo_addons/g_personnel/models/partner.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api,_
+
+class ResPartner(models.Model): 
+    _inherit = 'res.partner'
+    
+    province_id = fields.Many2one(comodel_name='res.province', string='Tỉnh/Thành')
+    district_id = fields.Many2one(comodel_name='res.district', string='Huyện/Quận')
+    ward_id = fields.Many2one(comodel_name='res.province', string='Xã/Phường')
