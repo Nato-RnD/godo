14097b97bf584aefc442eb14ca05ca3443852f32
Wong Golden <thiep.wong@gmail.com>
2022-08-29 00:58:32 +0700

    Cap nhat

--- godo_addons/g_production_place/controllers/main.py
+++ godo_addons/g_production_place/controllers/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from odoo import http 
-class GPucPortal(http.Controller):  
+from odoo.http import route,request 
+from odoo.addons.portal.controllers.portal import CustomerPortal
+class GPucPortal(CustomerPortal):  
     def _prepare_portal_layout_values(self):
         """Values for /my/* templates rendering.
 
@@ -8,7 +9,7 @@ class GPucPortal(http.Controller):
         """
         # get customer sales rep
         sales_user = False
-        partner = http.request.env.user.partner_id
+        partner = request.env.user.partner_id
         if partner.user_id and not partner.user_id._is_public():
             sales_user = partner.user_id
 
@@ -17,25 +18,75 @@ class GPucPortal(http.Controller):
             'page_name': 'home',
         }
         
-    @http.route(['/my', '/my/tokhai-kythuat'], type='http', auth="user", website=True)
+    @route(['/my', '/my/puc-register'], type='http', auth="user", website=True)
     def home(self, **kw):
         values = self._prepare_portal_layout_values()
-        _declarations = http.request.env['godo.production.unit.declaration'].search([('registered_user_id','=',http.request.env.user.id)])
+        _declarations = request.env['godo.production.unit.declaration'].sudo().search([('registered_user_id','=',request.env.user.id)])
         # decl = []
         # for model in _declarations:
         #     decl.append(model.read())
         values['test'] = _declarations
-        return http.request.render("g_production_place.portal_layout", values)
+        return request.render("g_production_place.portal_my_home", values)
 
  
-    @http.route(['/my/tokhai-kythuat/detail/<int:declaration_id>'], type='http', auth="user", website=True)
+    @route(['/my/puc-register/detail/<int:declaration_id>'], type='http', auth="user", website=True)
     def declaration_detail(self,declaration_id, **kw):
         values = self._prepare_portal_layout_values()
-        _declaration = http.request.env['godo.production.unit.declaration'].browse(declaration_id)
+        _declaration = request.env['godo.production.unit.declaration'].browse(declaration_id)
         # decl = []
         # for model in _declarations:
         #     decl.append(model.read())
         values['detail'] = _declaration
-        return http.request.render("g_production_place.portal_layout", values)
+        return request.render("g_production_place.portal_my_home", values)
 
+
+    @route(['/my/account'], type='http', auth='user', website=True)
+    def account(self, redirect=None, **post):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        values.update({
+            'error': {},
+            'error_message': [],
+        })
+
+        if post and request.httprequest.method == 'POST':
+            error, error_message = self.details_form_validate(post)
+            values.update({'error': error, 'error_message': error_message})
+            values.update(post)
+            if not error:
+                values = {key: post[key] for key in self.MANDATORY_BILLING_FIELDS}
+                values.update({key: post[key] for key in self.OPTIONAL_BILLING_FIELDS if key in post})
+                for field in set(['province_id', 'district_id']) & set(values.keys()):
+                    try:
+                        values[field] = int(values[field])
+                    except:
+                        values[field] = False
+                # values.update({'zip': values.pop('zipcode', '')})
+                partner.sudo().write(values)
+                if redirect:
+                    return request.redirect(redirect)
+                return request.redirect('/my/home')
+
+        provinces = request.env['res.province'].sudo().search([])
+        districts = request.env['res.district'].sudo().search([])
+        wards = request.env['res.ward'].sudo().search([])
+
+        values.update({
+            'partner': partner,
+            'provinces': provinces,
+            'districts': districts,
+            'wards': wards,
+            'has_check_vat': hasattr(request.env['res.partner'], 'check_vat'),
+            'redirect': redirect,
+            'page_name': 'my_details',
+        })
+
+        response = request.render("portal.portal_my_details", values)
+        response.headers['X-Frame-Options'] = 'DENY'
+        return response
  
+
+    @route(['/my', '/my/home'], type='http', auth="user", website=True)
+    def home(self, **kw):
+        values = self._prepare_portal_layout_values()
+        return request.render("portal.portal_my_home", values)
