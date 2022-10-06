eb2356876479244ea5ee00425154b4c7017ac1ea
Wong Golden <thiep.wong@gmail.com>
2022-10-06 17:07:08 +0700

    cap nhat

--- godo_addons/g_production_place/controllers/main.py
+++ godo_addons/g_production_place/controllers/main.py
@@ -42,12 +42,16 @@ class GPucPortal(CustomerPortal):
         values['declaration_detail'] = _declaration
         trees = request.env['godo.production.item'].sudo().search([])
         countries = request.env['res.country'].sudo().search([('target_country','=',True)]) 
+        farmers =  [farmer.strip() for farmer in  _declaration.farmers.strip().split(';') if farmer]
+        coords = [coord.strip() for coord in  _declaration.coordinates.strip().split(';') if coord]
         values.update({ 
             'declaration_detail': _declaration,
             'page_name': 'detail' ,
             'user': request.env.user,
             'countries': countries,
-            'trees': trees
+            'trees': trees,
+             'farmers': farmers,
+            'coords': coords
         })
         response = request.render("g_production_place.portal_my_puc_detailview", values)
         response.headers['X-Frame-Options'] = 'DENY'
@@ -104,9 +108,7 @@ class GPucPortal(CustomerPortal):
             'coords': coords
         })
              
-                 
-            print( farms_str, coord_str)
-            pass
+          
         
         response = request.render("g_production_place.portal_my_puc_detailview", values)
         response.headers['X-Frame-Options'] = 'DENY'
@@ -115,11 +117,51 @@ class GPucPortal(CustomerPortal):
     
     @route(['/my/productions/register'], type='http', auth="user", website=True)
     def declaration_register(self, **kw):
-        values = self._prepare_portal_layout_values()
-        # _declaration = request.env['godo.production.unit.declaration'].browse(declaration_id) 
+        values = self._prepare_portal_layout_values() 
+        trees = request.env['godo.production.item'].sudo().search([])
+        countries = request.env['res.country'].sudo().search([('target_country','=',True)])  
+        farms = {}
+        farms_str = []
+        coord_str =[]
+        coordinates = {} 
         values.update({  
-            'page_name': 'create' 
-        })
+            'page_name': 'create' ,
+            'user': request.env.user,
+            'countries': countries,
+            'trees': trees
+         })
+        
+        if kw and request.httprequest.method == 'POST':
+            for key,val in kw.copy().items():
+                if key.startswith('farmer_name_'):
+                    farms[key[12:]] = {'name': val} if key[12:] not in farms else dict(farms[key[12:]], name=val) # farms[key[12:]].update({'name': val})  
+                    del kw[key]
+                if key.startswith('farmer_area_'):
+                    farms[key[12:]] = {'area': val} if key[12:] not in farms else dict(farms[key[12:]], area=val) # farms[key[12:]].update({'area': val})  
+                    del kw[key]
+                if key.startswith('lat_'):
+                    coordinates[key[4:]] = {'lat': val} if key[4:] not in coordinates else dict(coordinates[key[4:]],lat=val)
+                    del kw[key]
+                if key.startswith('lng_'):
+                    coordinates[key[4:]] = {'lng': val} if key[4:] not in coordinates else dict(coordinates[key[4:]],lng=val)
+                    del kw[key] 
+
+            farms_str =  "; ".join('%s: %sha' % (v.get('name'),v.get('area')) for  v in farms.values())  
+            coord_str = "; ".join('%f,%f' % (float(v.get('lat')),float(v.get('lng'))) for  v in coordinates.values())  
+            kw.update({'farmers':farms_str, 'coordinates': coord_str, 'registered_user_id': request.env.user.id, 'registered_owner_type':'personal'})
+            del kw['username']  
+            try:
+                res = request.env['godo.production.unit.declaration'].sudo().create(kw)
+                request.env.cr.commit() 
+                if res:
+                    return request.redirect('/my/productions')
+                
+            except Exception as e:
+                pass
+                
+            
+             
+            
         return request.render("g_production_place.portal_my_puc_create_view", values)
 
 
@@ -176,8 +218,9 @@ class GPucPortal(CustomerPortal):
     
     
     @route('/my/productions/uploaded', type='http', auth="user",csrf=False, website=True)
-    def upload_files(self, **post):
-        values = {}
+    def upload_files(self,type,id, **post):
+     
+        _declaration = request.env['godo.production.unit.declaration'].browse(int(id))
         _file = post.get('file_data') 
         if _file:
             Attachments = request.env['ir.attachment']        
@@ -186,12 +229,18 @@ class GPucPortal(CustomerPortal):
                 'name':_file.filename,
                 'store_fname': _file.filename,
                 'res_name': _file.filename,
-                'type': 'binary',   
-                'res_model': 'godo.production.unit.declaration',
-                #'res_id': project_id,
+                'type': 'binary',    
                 'datas': base64.b64encode(attachment) #.encode('base64'),
             })
-            value = {
-                'attachment' : attachment_id
-            }
-            return value
+            
+            if attachment_id:
+                if type == 'cert':
+                    _declaration.write({
+                        'certificate_attachment_ids': [(4,attachment_id.id)]
+                    })
+                if type == 'agreement':
+                    _declaration.write({
+                        'agreement_attachment_ids': [(4,attachment_id.id)]
+                    }) 
+            response = request.render("g_production_place.upload_response", {'result': 'true' if attachment_id else 'false' }) 
+            return response
